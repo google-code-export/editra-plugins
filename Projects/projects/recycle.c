8b5cf66d92bf20b0d24cf5cd5b0c19fca1b7c828
Kevin.DaleSmith <Kevin.DaleSmith@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2007-08-16 03:25:50 +0000

    Had the wrong argument number in argv

--- Projects/projects/recycle.c
+++ Projects/projects/recycle.c
@@ -14,7 +14,7 @@ int main( int argc, char *argv[] )
     SHFILEOPSTRUCT sfo = {0};
 
     if ( !argc ) return( 0 );
-    if ( strlen(argv[0]) > 2048 ) return( 0 );
+    if ( strlen(argv[1]) > 2048 ) return( 0 );
 
 	sfo.hwnd = NULL;
 	sfo.wFunc = FO_DELETE;
@@ -22,8 +22,8 @@ int main( int argc, char *argv[] )
 	sfo.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | 
                  FOF_ALLOWUNDO | FOF_NOCONFIRMMKDIR;
 
-    strcpy( filename, argv[0] );
-    strcpy( filename + strlen(argv[0]), "\0\0" );
+    strcpy( filename, argv[1] );
+    strcpy( filename + strlen(argv[1]), "\0\0" );
 
 	return( SHFileOperation(&sfo) );
 }
