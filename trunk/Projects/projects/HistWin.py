4bdb18204ec0cbdca11e7688f372fef42467d11e
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2007-08-26 07:57:15 +0000

    Made a number of improvements to the UI as well as hooking the the listctrl selection events to put the log text in the TextCtrl.

--- trunk/Projects/projects/HistWin.py
+++ trunk/Projects/projects/HistWin.py
@@ -66,7 +66,6 @@ class HistoryWindow(wx.Frame):
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(self._ctrls, 1, wx.EXPAND)
         self.SetSizer(sizer)
-        self.SetAutoLayout(True)
 
 #-----------------------------------------------------------------------------#
 
@@ -78,7 +77,11 @@ class HistoryPane(wx.Panel):
         wx.Panel.__init__(self, parent)
         
         # Attributes
-        self._search = LogSearch(self)
+        sbox = wx.StaticBox(self, label=_("Revision History"))
+        # ?wxBug? If the box/sizer is created after the controls the controls
+        #         that are added to the box cannot get focus from mouse clicks
+        self.boxsz = wx.StaticBoxSizer(sbox, wx.VERTICAL)
+        self._search = LogSearch(self, size=(150, -1))
         self._split = wx.SplitterWindow(self, style=wx.SP_3DSASH | wx.SP_LIVE_UPDATE)
         self._list = HistList(self._split, data)
         self._txt = wx.TextCtrl(self._split, style=wx.TE_MULTILINE | wx.TE_READONLY)
@@ -90,6 +93,7 @@ class HistoryPane(wx.Panel):
 
         # Event Handlers
         self.Bind(wx.EVT_BUTTON, self.OnButton, self._btn)
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnItemSelected)
 
     def _DoLayout(self):
         """Layout the controls on the panel"""
@@ -100,13 +104,15 @@ class HistoryPane(wx.Panel):
         self._split.SetMinSize((400, 300))
         self._split.SetSashSize(8)
         self._split.SplitHorizontally(self._list, self._txt, 250)
+        self.boxsz.Add(self._search, 0, wx.ALIGN_RIGHT)
+        self.boxsz.Add((10, 10))
+        self.boxsz.Add(self._split, 1, wx.EXPAND)
 
         sizer.AddMany([((5, 5), (0, 0)), # Spacer
-                       (self._search, (1, 1), (1, 4), wx.EXPAND), # Search Control
-                       (self._split, (2, 1), (10, 10), wx.EXPAND), # Split Window
-                       ((5, 5), (11, 11)), # Spacer
-                       (self._btn, (12, 10), (1, 1)), # Button
-                       ((2, 2), (13, 0))]) # Spacer
+                       (self.boxsz, (1, 1), (10, 10), wx.EXPAND), # Split Window
+                       ((5, 5), (10, 11)), # Spacer
+                       (self._btn, (11, 10), (1, 1)), # Button
+                       ((2, 2), (12, 0))]) # Spacer
         self.SetSizer(sizer)
         self.SetAutoLayout(True)
 
@@ -119,6 +125,16 @@ class HistoryPane(wx.Panel):
             print "Diff to previous"
         else:
             print "Diff selected"
+
+    def OnItemSelected(self, evt):
+        """Update text control when an item is selected in the
+        list control.
+
+        """
+        index = evt.GetIndex()
+        rev = self._list.GetItem(index, self._list.REV_COL).GetText()
+        date = self._list.GetItem(index, self._list.DATE_COL).GetText()
+        self._txt.SetValue(self._list.GetFullLog(rev, date))
 
 #-----------------------------------------------------------------------------#
 
@@ -135,16 +151,26 @@ class HistList(wx.ListCtrl,
                                    wx.LC_VRULES)
 
         listmix.ListCtrlAutoWidthMixin.__init__(self)
+
+        # Attributes
+        self._data = data
+
+        # Setup columns
         self.InsertColumn(self.REV_COL, _("Rev #"))
         self.InsertColumn(self.DATE_COL, _("Date"))
         self.InsertColumn(self.AUTH_COL, _("Author"))
         self.InsertColumn(self.COM_COL, _("Log Message"))
-        self.Populate(data)
-#         self.SetColumnWidth(self.REV_COL, wx.LIST_AUTOSIZE)
-#         self.SetColumnWidth(self.AUTH_COL, wx.LIST_AUTOSIZE)
-#         self.SetColumnWidth(self.DATE_COL, wx.LIST_AUTOSIZE)
+        self.Populate(self._data)
         self.SetColumnWidth(self.COM_COL, wx.LIST_AUTOSIZE)
         self.SendSizeEvent()
+
+    def GetFullLog(self, rev, timestamp):
+        """Get the full log entry for the given revision"""
+        for item in self._data:
+            if item['revision'] == rev and item['date'] == timestamp:
+                return item['log']
+        else:
+            return wx.EmptyString
 
     def Populate(self, data):
         """Populate the list with the history data"""
@@ -152,11 +178,15 @@ class HistList(wx.ListCtrl,
             index = self.InsertStringItem(sys.maxint, item['revision'])
             self.SetStringItem(index, 1, item['date'])
             self.SetStringItem(index, 2, item['author'])
-            self.SetStringItem(index, 3, item['log'].strip())
-            if not index % 2:
+
+            log = item['log'].strip()
+            if len(log) > 45:
+                log = log[:45] + u'...'
+            self.SetStringItem(index, 3, log)
+            if index % 2:
                 syscolor = wx.SystemSettings_GetColour(wx.SYS_COLOUR_3DLIGHT)
-#                 color = util.AdjustColour(syscolor, 75)
-                self.SetItemBackgroundColour(index, syscolor)
+                color = AdjustColour(syscolor, 75)
+                self.SetItemBackgroundColour(index, color)
 
 #-----------------------------------------------------------------------------#
 
@@ -198,10 +228,36 @@ class LogSearch(wx.SearchCtrl):
         print "searching..."
 
 #-----------------------------------------------------------------------------#
+# Helper functions
+def AdjustColour(color, percent, alpha=wx.ALPHA_OPAQUE):
+    """ Brighten/Darken input colour by percent and adjust alpha
+    channel if needed. Returns the modified color.
+    @param color: color object to adjust
+    @type color: wx.Color
+    @param percent: percent to adjust +(brighten) or -(darken)
+    @type percent: int
+    @keyword alpha: amount to adjust alpha channel
+
+    """ 
+    end_color = wx.WHITE
+    rdif = end_color.Red() - color.Red()
+    gdif = end_color.Green() - color.Green()
+    bdif = end_color.Blue() - color.Blue()
+    high = 100
+
+    # We take the percent way of the color from color -. white
+    red = color.Red() + ((percent * rdif * 100) / high) / 100
+    green = color.Green() + ((percent * gdif * 100) / high) / 100
+    blue = color.Blue() + ((percent * bdif * 100) / high) / 100
+    return wx.Colour(red, green, blue, alpha)
+#-----------------------------------------------------------------------------#
 
 if __name__ == '__main__':
     data = [
         {'revision':'a', 'date':'2007/17/01', 'author':'Kevin Smith', 'log':'Just Testing'},
+        {'revision':'b', 'date':'2007/17/02', 'author':'Kevin Smith', 'log':'Test again with some longer text'},
+        {'revision':'c', 'date':'2007/17/03', 'author':'Kevin Smith', 'log':'Just Testing'},
+        {'revision':'d', 'date':'2007/17/04', 'author':'Kevin Smith', 'log':'Log message with lots of text to test the truncation of long messages and their display in the text control.'}
     ]
     app = wx.PySimpleApp(False)
     win = HistoryWindow(None, "History Window Test", data)
