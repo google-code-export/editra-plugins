e04a29a0bf391d3db44f2e3f2f4e5cb0e73ad058
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2007-10-21 02:37:22 +0000

    Move css optimizer plugin to editra-plugins repository

---
+++ trunk/cssoptimizer/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Setup script to build the CssOptimizer plugin. To build the plugin
+# just run 'python setup.py' and an egg will be built and put into 
+# the plugin directory
+"""Creates an Editra Generator for optimizing CSS Files"""
+
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    print "You must have setup tools installed in order to build this plugin"
+    setup = None
+
+__author__ = "Cody Precord"
+
+if setup != None:
+    setup(
+        name='CssOptimizer',
+        version='0.2',
+        description=__doc__,
+        author=__author__,
+        author_email="cprecord@editra.org",
+        license="wxWindows",
+        url="http://editra.org",
+        platforms=["Linux", "OS X", "Windows"],
+        packages=['cssoptimizer'],
+        entry_points='''
+        [Editra.plugins]
+        CssOptimizer = cssoptimizer:CssOptimizer
+        '''
+        )
