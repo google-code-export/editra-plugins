b9c61e9b0b8499ffc1279d5712f097ecef67cbe2
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-01-12 13:27:40 +0000

    lint and code cleanup

--- trunk/Projects/projects/__init__.py
+++ trunk/Projects/projects/__init__.py
@@ -7,58 +7,57 @@ __revision__ = "$Revision$"
 __scid__ = "$Id$"
 __version__ = "0.1"
 
+#-----------------------------------------------------------------------------#
+# Imports
 import wx 
-import stat
-import os 
-import time
-import fnmatch
-import threading
 import plugin 
-import string
 import ed_glob
 import iface
 import ed_menu
 from profiler import Profile_Get, Profile_Set
 from ProjectPane import ProjectPane
 
+#-----------------------------------------------------------------------------#
+# Globals
 _ = wx.GetTranslation
 PANE_NAME = ProjectPane.PANE_NAME
 
-class Logger(object):
-    def WriteText(self, *args):
-        pass
+#-----------------------------------------------------------------------------#
 
 class Projects(plugin.Plugin):
     """Adds a projects pane to the view menu"""
     plugin.Implements(iface.MainWindowI)
     def PlugIt(self, parent):
         """Adds the view menu entry and registers the event handler"""
-        mw = parent
+        mainw = parent
         self._log = wx.GetApp().GetLog()
-        if mw != None:
+        if mainw != None:
             self._log("[projects] Installing projects plugin")
 
-            self._projects = ProjectPane(mw)
-            mw._mgr.AddPane(self._projects, wx.aui.AuiPaneInfo().Name(PANE_NAME).\
-                            Caption("Projects").Left().Layer(1).\
-                            CloseButton(True).MaximizeButton(False).\
-                            BestSize(wx.Size(215, 350)))
+            self._projects = ProjectPane(mainw)
+            mgr = mainw.GetFrameManager()
+            mgr.AddPane(self._projects, wx.aui.AuiPaneInfo().Name(PANE_NAME).\
+                        Caption("Projects").Left().Layer(1).\
+                        CloseButton(True).MaximizeButton(False).\
+                        BestSize(wx.Size(215, 350)))
 
             # Get settings from profile
             if Profile_Get('Projects.Show', 'bool', False):
-                mw._mgr.GetPane(PANE_NAME).Show()
+                mgr.GetPane(PANE_NAME).Show()
             else:
-                mw._mgr.GetPane(PANE_NAME).Hide()
+                mgr.GetPane(PANE_NAME).Hide()
 
-            mw._mgr.Update()
+            mgr.Update()
 
             # Event Handlers
-            mw.Bind(wx.aui.EVT_AUI_PANE_CLOSE, self.OnPaneClose)
+            mainw.Bind(wx.aui.EVT_AUI_PANE_CLOSE, self.OnPaneClose)
 
     def GetMenuHandlers(self):
+        """Returns the menu event handlers"""
         return [(self._projects.ID_PROJECTS, self._projects.OnShowProjects)]
 
     def GetUIHandlers(self):
+        """Returns handlers for UpdateUI events"""
         return list()
 
     def OnPaneClose(self, evt):
