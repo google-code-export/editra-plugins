9373b27879e35dd94d0eda3d86fcf15a50ee46bf
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-01-13 03:29:23 +0000

    Lint and code cleanup

--- trunk/Projects/projects/SVN.py
+++ trunk/Projects/projects/SVN.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+"""Subversion implementation of the SourceControl object """
+
 __author__ = "Kevin D. Smith <Kevin.Smith@sixquickrun.com>"
 __revision__ = "$Revision$"
 __scid__ = "$Id$"
@@ -8,7 +10,7 @@ import re, os, crypto, datetime
 from SourceControl import SourceControl
 
 class SVN(SourceControl):
-
+    """ Subversion source control class """
     name = 'Subversion'
     command = 'svn'
 
@@ -16,6 +18,7 @@ class SVN(SourceControl):
         return 'SVN.SVN()'
         
     def getAuthOptions(self, path):
+        """ Get the repository authentication info """
         output = []
         options = self.getRepositorySettings(path)
         if options.get('username', ''):
@@ -27,6 +30,7 @@ class SVN(SourceControl):
         return output
     
     def getRepository(self, path):
+        """ Get the repository of a given path """
         if not self.isControlled(path):
             return
         if not os.path.isdir(path):
@@ -45,15 +49,16 @@ class SVN(SourceControl):
     def isControlled(self, path):
         """ Is the path controlled by CVS? """
         if os.path.isdir(path):
-            if os.path.isfile(os.path.join(path,'.svn','entries')):
+            if os.path.isfile(os.path.join(path, '.svn', 'entries')):
                 return True
         path, basename = os.path.split(path)
         svndir = os.path.join(path,'.svn')
         if os.path.isdir(svndir):
             try:
                 entries = open(os.path.join(svndir,'entries')).read()
-                entries = [x.strip().split('\n')[0] for x in entries.split('\x0c') 
-                                                    if x.strip()][1:]
+                entries = [x.strip().split('\n')[0]
+                           for x in entries.split('\x0c') 
+                           if x.strip()][1:]
                 return basename in entries
             except (IOError, OSError):
                 pass
@@ -72,8 +77,9 @@ class SVN(SourceControl):
         self.logOutput(out)
         
     def checkout(self, paths):
+        """ Checkout files at the given path """
         root, files = self.splitFiles(paths)
-        out = self.run(root, ['checkout','--non-interactive'] + self.getAuthOptions(root) + files)
+        out = self.run(root, ['checkout', '--non-interactive'] + self.getAuthOptions(root) + files)
         self.logOutput(out)
         
     def commit(self, paths, message=''):
@@ -83,23 +89,27 @@ class SVN(SourceControl):
         self.logOutput(out)
                                    
     def diff(self, paths):
+        """ Run the diff program on the given files """
         root, files = self.splitFiles(paths)
-        out = self.run(root, ['diff','--non-interactive'] + self.getAuthOptions(root) + files)
+        out = self.run(root, ['diff', '--non-interactive'] + self.getAuthOptions(root) + files)
         self.closeProcess(out)
         
     def history(self, paths, history=None):
+        """ Get the revision history of the given paths """
         if history is None:
             history = []
+
         root, files = self.splitFiles(paths)
-        for file in files:
-            out = self.run(root, ['log','--non-interactive'] + self.getAuthOptions(root) + [file])
+        for fname in files:
+            out = self.run(root, ['log', '--non-interactive'] + \
+                                  self.getAuthOptions(root) + [fname])
             pophistory = False
             if out:
                 for line in out.stdout:
                     self.log(line)
                     if line.strip().startswith('-----------'):
                         pophistory = True
-                        current = {'path':file}
+                        current = {'path':fname}
                         history.append(current)
                         for data in out.stdout:
                             self.log(data)
@@ -118,15 +128,17 @@ class SVN(SourceControl):
         return history
     
     def str2datetime(self, s):
+        """ Convert a timestamp string to a datetime object """
         return datetime.datetime(*[int(y) for y in [x for x in re.split(r'[\s+/:-]', s.strip()) if x][:6]])
         
     def remove(self, paths):
         """ Recursively remove paths from repository """
         root, files = self.splitFiles(paths)
-        out = self.run(root, ['remove','--force'] + self.getAuthOptions(root) + files)
+        out = self.run(root, ['remove', '--force'] + \
+                       self.getAuthOptions(root) + files)
         self.logOutput(out)
         
-    def status(self, paths, recursive=False, status={}):
+    def status(self, paths, recursive=False, status=dict()):
         """ Get SVN status information from given file/directory """
         codes = {' ':'uptodate', 'A':'added', 'C':'conflict', 'D':'deleted',
                  'M':'modified'}
@@ -154,7 +166,8 @@ class SVN(SourceControl):
     def update(self, paths):
         """ Recursively update paths """
         root, files = self.splitFiles(paths)
-        out = self.run(root, ['update','--non-interactive'] + self.getAuthOptions(root) + files)
+        out = self.run(root, ['update', '--non-interactive'] + \
+                              self.getAuthOptions(root) + files)
         self.logOutput(out)
             
     def revert(self, paths):
@@ -162,7 +175,7 @@ class SVN(SourceControl):
         root, files = self.splitFiles(paths)
         if not files:
             files = ['.']
-        out = self.run(root, ['revert','-R'] + files)
+        out = self.run(root, ['revert', '-R'] + files)
         self.logOutput(out)
             
     def fetch(self, paths, rev=None, date=None):
@@ -183,7 +196,8 @@ class SVN(SourceControl):
                 options.append('-r')
                 options.append('{%s}' % date)
             
-            out = self.run(root, ['cat','--non-interactive'] + options + self.getAuthOptions(root) + files)
+            out = self.run(root, ['cat', '--non-interactive'] + \
+                                  options + self.getAuthOptions(root) + files)
             if out:
                 output.append(out.stdout.read())
                 self.logOutput(out)
@@ -194,7 +208,8 @@ class SVN(SourceControl):
     @property
     def salt(self):
         return '"\x17\x9f/D\xcf'
-               
+
+#-----------------------------------------------------------------------------#
 if __name__ == '__main__':
     svn = SVN()
     svn.add(['/Users/kesmit/pp/editra-plugins/Projects/projects/Icons'])
