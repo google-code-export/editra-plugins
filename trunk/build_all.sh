709cec30f49ade79e4e29435b0061e3102936253
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-01-22 08:43:51 +0000

    Add clean option to build script

--- trunk/build_all.sh
+++ trunk/build_all.sh
@@ -17,6 +17,11 @@ YELLOW="[33;01m"
 OFF="[0m"
 
 ### Helper Functions ###
+
+###############################################################################
+# print_help
+# Print help message of available commands
+###############################################################################
 print_help () {
     echo "Editra Plugins Build Script"
     echo "Type 'build_all.sh [hv]' to run a build command"
@@ -24,72 +29,122 @@ print_help () {
     echo "Available Options:"
     echo "  -h      Print This help message"
     echo "  -v      Verbose build information"
+    echo "  -c      Clean all temporary build files"
     echo ""
 }
 
+###############################################################################
+# clean_all
+# Do a clean of all build files
+###############################################################################
+clean_all () {
+    shopt -s extglob
+    DELCMD="rm -rf"
+
+    # Clean all built plugins from this directory
+    echo "${CYAN}Starting Clean${OFF}"
+    echo "${GREEN}Cleaning${OFF} toplevel directory"
+    for plugin in $(ls); do
+        if ! [ -z `echo $plugin | grep '.*\.egg\|pyc\|pyo\|~'` ]; then
+            echo "${RED}  rm${OFF} $plugin"
+            rm $plugin
+        fi
+    done
+
+    # Clean all subdirectories
+    for plugin in $(ls); do
+        if [ -d "$plugin" ]; then
+            echo "${GREEN}Cleaning${OFF} $plugin...";
+            cd $plugin
+            for dir in $(ls); do
+                RES=`echo $dir | grep 'dist\|build\|.*\.egg-info'`
+                if ! [ -z "$RES" ]; then
+                    echo "  ${RED}${DELCMD}${OFF} $plugin/$dir"
+                    `$DELCMD $dir`
+                fi
+            done
+            cd ..
+        fi
+    done
+}
+
+###############################################################################
+# build_all
+# Build all plugins using the setup.py files found in the sub
+# directories of this directory. All dist (egg) files are placed
+# in this directory.
+###############################################################################
+build_all () {
+    # Commands
+    if [ $VERBOSE -ne 0 ]; then
+        BUILD="setup.py bdist_egg --dist-dir=../."
+    else
+        BUILD="setup.py --quiet bdist_egg --dist-dir=../."
+    fi
+
+    ## Check if both python 2.4 and 2.5 are available ##
+    python2.5 -V 2>/dev/null
+    if [ $? -eq 0 ]; then
+       PY25="python2.5"
+    fi
+
+    python2.4 -V 2>/dev/null
+    if [ $? -eq 0 ]; then
+        PY24="python2.4"
+    fi
+
+    # Abort if no suitable python is found
+    if [[ -z "$PY25" && -z "$PY24" ]]; then
+        echo "${RED}!!${OFF} Neither Python 2.4 or 2.5 could be found ${RED}!!${OFF}"
+        echo "${RED}!!${OFF} Aborting build ${RED}!!${OFF}"
+        exit
+    fi
+
+    #### Do the Builds ####
+    echo ""
+    echo "${YELLOW}**${OFF} Enumerating all plugins... ${YELLOW}**${OFF} "
+    echo ""
+
+    for plugin in $(ls); do
+        if [ -d "$plugin" ]; then
+            echo "${GREEN}Building${OFF} $plugin...";
+            cd $plugin
+            if [ -n "$PY25" ]; then
+                echo "${CYAN}Python2.5${OFF} Building..";
+                `$PY25 $BUILD`
+                echo "${CYAN}Python2.5${OFF} Build finished"
+            fi
+            if [ -n "$PY24" ]; then
+                echo "${CYAN}Python2.4${OFF} Building..";
+                `$PY24 $BUILD`
+                echo "${CYAN}Python2.4${OFF} Build finished"
+            fi
+            cd ..
+            echo ""
+        fi
+    done
+
+    echo ""
+    echo "${YELLOW}**${OFF} Finished Building all plugins ${YELLOW}**${OFF}"
+    echo ""
+
+}
+
 # Parse command line args and set associated params
-while getopts "hv" flag
+while getopts "hvc" flag
 do
-    if [[ "$flag" == "v" ]]; then
-        VERBOSE=1
-    elif [[ "$flag" == "h" ]]; then
+    if [[ "$flag" == "h" ]]; then
         print_help
+        exit
+    elif [[ "$flag" == "v" ]]; then
+        VERBOSE=1
+    elif [[ "$flag" == "c" ]]; then
+        clean_all
         exit
     else
         continue
     fi
 done
 
-# Commands
-if [ $VERBOSE -ne 0 ]; then
-    BUILD="setup.py bdist_egg --dist-dir=../."
-else
-    BUILD="setup.py --quiet bdist_egg --dist-dir=../."
-fi
-
-## Check if both python 2.4 and 2.5 are available ##
-python2.5 -V 2>/dev/null
-if [ $? -eq 0 ]; then
-   PY25="python2.5"
-fi
-
-python2.4 -V 2>/dev/null
-if [ $? -eq 0 ]; then
-    PY24="python2.4"
-fi
-
-# Abort if no suitable python is found
-if [[ -z "$PY25" && -z "$PY24" ]]; then
-    echo "${RED}!!${OFF} Neither Python 2.4 or 2.5 could be found ${RED}!!${OFF}"
-    echo "${RED}!!${OFF} Aborting build ${RED}!!${OFF}"
-    exit
-fi
-
-#### Do the Builds ####
-echo ""
-echo "${YELLOW}**${OFF} Enumerating all plugins... ${YELLOW}**${OFF} "
-echo ""
-
-for plugin in $(ls); do
-    if [ -d "$plugin" ]; then
-        echo "${GREEN}Building${OFF} $plugin ...";
-        cd $plugin
-        if [ -n "$PY25" ]; then
-            echo "${CYAN}Python2.5${OFF} Building..";
-            `$PY25 $BUILD`
-            echo "${CYAN}Python2.5${OFF} Build finished"
-        fi
-        if [ -n "$PY24" ]; then
-            echo "${CYAN}Python2.4${OFF} Building..";
-            `$PY24 $BUILD`
-            echo "${CYAN}Python2.4${OFF} Build finished"
-        fi
-        cd ..
-        echo ""
-    fi
-done
-
-echo ""
-echo "${YELLOW}**${OFF} Finished Building all plugins ${YELLOW}**${OFF}"
-echo ""
-
+# Do the build if no other command have been specified
+build_all
