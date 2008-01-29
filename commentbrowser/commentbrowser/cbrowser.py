7dd4ca0c11bcc3fa69c2d0fbaed8466f583c9237
dr0iddr0id <dr0iddr0id@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-01-29 10:27:38 +0000

    added 'note:' as keyword like 'todo:'

--- commentbrowser/commentbrowser/cbrowser.py
+++ commentbrowser/commentbrowser/cbrowser.py
@@ -50,7 +50,7 @@ ID_TIMER = wx.NewId()
 
 #[low priority, ..., high priority]
 
-TASK_CHOICES = ['ALL', 'TODO', 'HACK', 'XXX', 'FIXME']
+TASK_CHOICES = ['ALL', 'NOTE', 'TODO', 'HACK', 'XXX', 'FIXME']
 
 RE_TASK_CHOICES = []
 for task in TASK_CHOICES:
