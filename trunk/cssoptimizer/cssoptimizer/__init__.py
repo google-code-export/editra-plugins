e30c25224e29a6719854afaa6af63829a684b9f0
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-08-20 12:45:54 +0000

    Update version

--- trunk/cssoptimizer/cssoptimizer/__init__.py
+++ trunk/cssoptimizer/cssoptimizer/__init__.py
@@ -9,7 +9,7 @@
 # Plugin Metadata
 """Generate optimized Css code"""
 __author__ = "Cody Precord"
-__version__ = "0.3"
+__version__ = "0.4"
 
 #-----------------------------------------------------------------------------#
 # Imports
