d113e522f4f91d36282e81222dcc7440104f87c0
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-11-08 21:54:45 +0000

    Stub in start of a python debugger plugin

---
+++ pydebugger/pydebugger/__init__.py
@@ -0,0 +1,27 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: PyDebugger plugin for integrating a Python debugger in Editra      #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+""" PyDebugger
+Plugin to integrate a debugger for python.
+
+@summary: Integrates a Python debugging in Editra
+
+"""
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#-----------------------------------------------------------------------------#
+
+# Editra Libraries
+import plugin
+
+#-----------------------------------------------------------------------------#
+
+class PyDebugger(plugin.Plugin):
+    """Python Debugger Plugin"""
