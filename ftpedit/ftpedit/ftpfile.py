cdefb8d1f23c8da63532a5c892c0f578362ce0f3
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2008-12-28 05:46:00 +0000

    * Add FtpFile to start integration with open files over ftp in Editra. * Start Download/Upload code * Fix some issues with reusing the same list control after disconnecting. * Fix config items not getting saved under some use cases * Enable using default path setting

---
+++ ftpedit/ftpedit/ftpfile.py
@@ -0,0 +1,101 @@
+###############################################################################
+# Name: ftpfile.py                                                            #
+# Purpose: Ftp file layer                                                     #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+"""Ftp File
+
+Classes and utilities for abstracting files operations over ftp.
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#-----------------------------------------------------------------------------#
+# Imports
+import os
+
+# Editra Libraries
+import ed_txt
+from util import Log
+
+# Local Imports
+import ftpclient
+
+#-----------------------------------------------------------------------------#
+
+class FtpFile(ed_txt.EdFile):
+    def __init__(self, ftppath, sitedata, path='', modtime=0):
+        """Create the FtpFile
+        @param ftppath: path to file on ftp server
+        @param sitedata: site login data
+        @keyword path: on disk path (used by EdFile)
+        @keyword modtime: last mod time (used by EdFile)
+
+        """
+        ed_txt.EdFile.__init__(self, path, modtime)
+
+        # Attributes
+        self._client = ftpclient.FtpClient(None)
+        self.ftppath = ftppath
+        self._site = sitedata   # dict(url, port, user, pword, path, enc)
+
+        # Setup
+        self.SetEncoding(enc)
+
+    def __del__(self):
+        """Cleanup the temp file"""
+        os.remove(self.GetPath())
+        if self._client.IsActive():
+            self._client.Disconnect()
+
+    def DoFtpUpload(self):
+        """Upload the contents of the on disk temp file to the server"""
+        connected = self._client.IsActive()
+        if not connected:
+            self._client.SetHostname(self._site['url'])
+            self._client.SetPort(self._site['port'])
+            connected = self._client.Connect(self._site['user'],
+                                             self._site['pword'])
+
+        if not connected:
+            # TODO: report error to upload
+            err = self._client.GetLastError()
+            Log("[ftpedit][err] DoFtpUpload: %s" % err)
+        else:
+            self._client.Upload(self.GetPath, self.ftppath)
+            # TODO: notify of completion
+
+    def GetFtpPath(self):
+        """Get the ftp path
+        @return: string
+
+        """
+        return self.ftppath
+
+    def GetSiteData(self):
+        """Get the ftp site data that this file belongs to
+        @return: dict(url, port, user, pword, path, enc)
+
+        """
+        return self._site
+
+    def Write(self, value):
+        """Override EdFile.Write to trigger an upload
+        @param value: string
+
+        """
+        # Save the local file
+        super(ed_txt.EdFile, self).Write(value)
+
+        # TODO: fire upload thread
+        
+
+#-----------------------------------------------------------------------------#
+
+
