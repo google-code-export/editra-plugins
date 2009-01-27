dbdc0b063fd0658e972607c49e577ea262709f6e
cowo78 <cowo78@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-01-27 09:27:02 +0000

    Derived list control from ListCtrlAutoWidthMixin and ListRowHighlighter Moved syntax checkers to modules

---
+++ SyntaxCheck/SyntaxCheck/PythonSyntaxChecker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Name: php.py
+# Purpose: Syntax Checker plugin
+# Author: Giuseppe "Cowo" Corbelli
+# Copyright: (c) 2009 Giuseppe "Cowo" Corbelli
+# License: wxWindows License
+# Plugin Metadata
+""" Syntax checker module for PHP data
+"""
+__version__ = "0.1"
+
+__author__ = "Giuseppe 'Cowo' Corbelli"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+import AbstractSyntaxChecker
+
+class PythonSyntaxChecker(AbstractSyntaxChecker.AbstractSyntaxChecker):
+    @staticmethod
+    def Check(fileName):
+        try:
+            fd = open(fileName, 'r')
+            code = fd.read().replace('\r\n', '\n').replace('\r', '\n')
+            compile(code, fileName, 'exec')
+        except SyntaxError, e:
+            return [ ("Syntax Error", e.text.rstrip(), e.lineno) ]
+        except IndentationError, e:
+            return [ ("Indentation Error", e.text.rstrip(), e.lineno) ]
+        except TypeError, e:
+            return [ ("Type Error", "Source contains NULL bytes", None) ]
+        except Exception, e:
+            return [ ("Unknown Error", str(e), None) ]
+
+        return []
