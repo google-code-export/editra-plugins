9aaf9812d92dfc8964defe27e5a6ce1db2f5eb6c
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-01-28 04:28:09 +0000

    Enhance commit dialog to remember recent messages to make it easier to quickly re enter a message if the commit fails for some reason.

--- trunk/Projects/projects/ProjCmnDlg.py
+++ trunk/Projects/projects/ProjCmnDlg.py
@@ -49,6 +49,9 @@ def RetrievalErrorDlg(parent):
 
 class CommitDialog(wx.Dialog):
     """Dialog for entering commit messages"""
+    RECENT_MESSAGES = [u'',]
+    _TEASER_LIST = [u'',]
+
     def __init__(self, parent, title=u'', caption=u'', default=list()):
         """Create the Commit Dialog
         @keyword default: list of file names that are being commited
@@ -57,7 +60,7 @@ class CommitDialog(wx.Dialog):
         wx.Dialog.__init__(self, parent, title=title)
 
         # Attributes
-        self._caption = wx.StaticText(self, label=caption)
+        self._recent = wx.Choice(self, choices=CommitDialog._TEASER_LIST)
         self._commit = wx.Button(self, wx.ID_OK, _("Commit"))
         self._commit.SetDefault()
         self._cancel = wx.Button(self, wx.ID_CANCEL)
@@ -68,6 +71,7 @@ class CommitDialog(wx.Dialog):
         if wx.Platform == '__WXMAC__':
             font.SetPointSize(12)
             self._entry.MacCheckSpelling(True)
+            self._recent.SetWindowVariant(wx.WINDOW_VARIANT_SMALL)
         else:
             font.SetPointSize(10)
         self._entry.SetFont(font)
@@ -78,6 +82,10 @@ class CommitDialog(wx.Dialog):
         # Layout
         self._DoLayout()
         self.CenterOnParent()
+
+        # Event Handlers
+        self.Bind(wx.EVT_BUTTON, self.OnOk, id=wx.ID_OK)
+        self.Bind(wx.EVT_CHOICE, self.OnChoice)
 
     def _DefaultMessage(self, files):
         """
@@ -100,34 +108,73 @@ class CommitDialog(wx.Dialog):
     def _DoLayout(self):
         """ Used internally to layout dialog before being shown """
         sizer = wx.BoxSizer(wx.VERTICAL)
-        csizer = wx.BoxSizer(wx.HORIZONTAL)
-        bsizer = wx.BoxSizer(wx.HORIZONTAL)
+        bsizer = wx.StdDialogButtonSizer()
         esizer = wx.BoxSizer(wx.HORIZONTAL)
+        rsizer = wx.BoxSizer(wx.HORIZONTAL)
 
         sizer.Add((10, 10), 0)
-        csizer.Add((10, 10), 0)
-        csizer.Add(self._caption, 0, wx.ALIGN_LEFT, 5)
-        sizer.Add(csizer, 0)
+        rtxt = wx.StaticText(self, label=_("Recent Messages") + u":")
+        rsizer.AddMany([((10, 10), 0), (rtxt, 0, wx.ALIGN_LEFT, 5),
+                        ((5, 5), 0), (self._recent, 1, wx.EXPAND),
+                        ((10, 10), 0)])
+        self._recent.Enable(self._recent.GetCount() > 1)
+        sizer.Add(rsizer, 0, wx.EXPAND)
         sizer.Add((10, 10), 0)
+
         esizer.AddMany([((10, 10), 0),
                         (self._entry, 1, wx.EXPAND),
                         ((10, 10), 0)])
         sizer.Add(esizer, 0, wx.EXPAND)
-        bsizer.AddStretchSpacer()
-        bsizer.AddMany([(self._cancel, 0, wx.ALIGN_RIGHT, 5), ((5, 5)),
-                       (self._commit, 0, wx.ALIGN_RIGHT, 5), ((5, 5))])
-        sizer.Add((10, 10))
+
+        bsizer.AddButton(self._cancel)
+        bsizer.AddButton(self._commit)
+        bsizer.Realize()
+        sizer.Add((8, 8), 0)
         sizer.Add(bsizer, 0, wx.ALIGN_RIGHT)
-        sizer.Add((10, 10))
+        sizer.Add((8, 8), 0)
         self.SetSizer(sizer)
         self.SetInitialSize()
 
     def GetValue(self):
         """Return the value of the commit message"""
-        txt = self._entry.GetString(0, self._entry.GetLastPosition()).strip()
+        txt = self._entry.GetValue().strip()
         txt = txt.replace('\r\n', '\n')
         return os.linesep.join([ x for x in txt.split('\n')
                                  if not x.lstrip().startswith(':') ])
+
+    def OnChoice(self, evt):
+        """Insert message from recently selected list"""
+        idx = evt.GetSelection()
+        if idx < len(CommitDialog.RECENT_MESSAGES):
+            msg = CommitDialog.RECENT_MESSAGES[idx]
+            self.SetCommitMessage(msg)
+        else:
+            evt.Skip()
+
+    def OnOk(self, evt):
+        """Handle click events from the commit button"""
+        txt = self.GetValue()
+        if txt and txt not in CommitDialog.RECENT_MESSAGES:
+            CommitDialog.RECENT_MESSAGES.insert(1, txt)
+            if len(txt) > 25:
+                teaser = txt[:25] + u"..."
+            else:
+                teaser = txt
+            CommitDialog._TEASER_LIST.insert(1, teaser)
+        evt.Skip()
+
+    def SetCommitMessage(self, msg):
+        """Set the commit message
+        @param msg: string
+
+        """
+        # Get the header
+        txt = self._entry.GetValue().strip()
+        txt = txt.replace('\r\n', '\n')
+        header = os.linesep.join([x for x in txt.split('\n')
+                                  if x.strip().startswith(':')])
+        self._entry.SetValue(header + os.linesep + msg)
+        self._entry.SetInsertionPoint(self._entry.GetLastPosition())
 
 #-----------------------------------------------------------------------------#
 
