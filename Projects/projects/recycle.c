40a1efb1a81b50c2db76d6e8d0e1112801262406
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-01-31 00:41:48 +0000

    Add code for new windows recycler from rudi.pettazzi. (not yet tested or recompiled to recycle.exe.)

--- Projects/projects/recycle.c
+++ Projects/projects/recycle.c
@@ -1,3 +1,51 @@
+/** 
+ * Compile with mingw32-gcc -Wall -Wl,--subsystem,windows -DUNICODE recycle.c -o recycle.exe 
+ *
+ * Delete the given file or directory (an absolute path passed as argv[1]).
+ * Returns 1 if the argument is invalid, or SHFileOperation return value (0 if successful, nonzero otherwise)
+ */
+
+#include <windows.h>
+#pragma warning(disable : 4996)
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)     
+{
+    wchar_t filename[MAX_PATH + 1] = { 0 };
+    SHFILEOPSTRUCT sfo;
+    size_t len = 0;
+    int wargc = 0;
+    int ret = 0;
+    wchar_t **wargv = CommandLineToArgvW(GetCommandLineW(), &wargc);
+
+    if (wargv == 0 || wargc < 2)
+      return 1;
+
+    len = wcslen(wargv[1]);
+
+    if (len > MAX_PATH)
+        return 1;
+
+    wcscpy(filename, wargv[1]);
+    wcscpy(filename + wcslen(filename) + 1, L"\0");
+
+    ZeroMemory(&sfo, sizeof(sfo));
+
+    sfo.hwnd = NULL;
+    sfo.wFunc = FO_DELETE;
+    sfo.pFrom = filename;
+
+    /* XXX maybe FOF_NOERRORUI should be off */
+    sfo.fFlags = FOF_SILENT | FOF_NOERRORUI | FOF_ALLOWUNDO | FOF_NOCONFIRMMKDIR | FOF_NOCONFIRMATION;
+
+    /* 0 if successful, nonzero otherwise. */
+    ret = SHFileOperation(&sfo);
+
+    LocalFree(wargv);
+
+    return ret;   
+}
+
+
 //
 // Command-line utility to send a file or directory to the Recycle Bin
 //
@@ -5,30 +53,25 @@
 // That argument must be a full path to the file.
 //
 
-// Author: Kevin D. Smith <Kevin.Smith@sixquickrun.com>
-// $Revision$
-// $Id$
+/*#include <windows.h>*/
+/*#include <string.h>*/
 
+/*int main( int argc, char *argv[] )*/
+/*{*/
+/*	char filename[2048] = {0};*/
+/*    SHFILEOPSTRUCT sfo = {0};*/
 
-#include <windows.h>
-#include <string.h>
+/*    if ( !argc ) return( 0 );*/
+/*    if ( strlen(argv[1]) > 2048 ) return( 0 );*/
 
-int main( int argc, char *argv[] )
-{
-	char filename[2048] = {0};
-    SHFILEOPSTRUCT sfo = {0};
+/*	sfo.hwnd = NULL;*/
+/*	sfo.wFunc = FO_DELETE;*/
+/*	sfo.pFrom = filename;*/
+/*	sfo.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | */
+/*                 FOF_ALLOWUNDO | FOF_NOCONFIRMMKDIR;*/
 
-    if ( !argc ) return( 0 );
-    if ( strlen(argv[1]) > 2048 ) return( 0 );
+/*    strcpy( filename, argv[1] );*/
+/*    strcpy( filename + strlen(argv[1]), "\0\0" );*/
 
-	sfo.hwnd = NULL;
-	sfo.wFunc = FO_DELETE;
-	sfo.pFrom = filename;
-	sfo.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | 
-                 FOF_ALLOWUNDO | FOF_NOCONFIRMMKDIR;
-
-    strcpy( filename, argv[1] );
-    strcpy( filename + strlen(argv[1]), "\0\0" );
-
-	return( SHFileOperation(&sfo) );
-}
+/*	return( SHFileOperation(&sfo) );*/
+/*}*/
