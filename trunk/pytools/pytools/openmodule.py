5b89e7c1b3d7591d24754173077c2fccff9a6244
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-02-02 16:26:26 +0000

    Fix some UI issues and improve layout. Minor cleanup for some style related items.

--- trunk/pytools/pytools/openmodule.py
+++ trunk/pytools/pytools/openmodule.py
@@ -6,66 +6,91 @@
 # License:                                                                    #
 ###############################################################################
 
-import wx
-import sys
-import os.path
+"""Open module Dialog"""
 
 __author__ = ""
 __svnid__ = "$Id$"
 __revision__ = "$Revision$"
 
+#-----------------------------------------------------------------------------#
+# Imports
+import wx
+import sys
+import os.path
+
+#-----------------------------------------------------------------------------#
+# Globals
 _ = wx.GetTranslation
+
+#-----------------------------------------------------------------------------#
 
 class OpenModuleDialog(wx.Dialog):
     """ A dialog to find the source file of a module and open it into the
     editor notebook
     """
+    # TODO: the extensions for source files could also be retrieved from
+    #       syntax.syntax.ExtensionRegister
     _SRC_EXTENSIONS = '.py', '.pyw'
     _BYTECODE_EXTENSIONS = '.pyc', '.pyo'
 
     def __init__(self, parent, caption=u'', *args, **kwargs):
-        wx.Dialog.__init__(self, parent, -1, caption, style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER,
-            size=(400, 230), *args, **kwargs)
+        wx.Dialog.__init__(self, parent, wx.ID_ANY, caption,
+                           style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER,
+                           size=(400, 230), *args, **kwargs)
 
-        self.label1 = wx.StaticText(self, -1, _('Find and open a module by name'))
+        # Attributes
         self.search = wx.SearchCtrl(self, style=wx.TE_PROCESS_ENTER)
         self.search.ShowCancelButton(True)
-        self.label2 = wx.StaticText(self, -1, _('Matching items:'))
-        self.btnOk = wx.Button(self, label=_('Ok'))
+        self.btnOk = wx.Button(self, wx.ID_OK, label=_('Ok'))
         self.btnOk.Enable(False)
-        self.btnCancel = wx.Button(self, label=_('Cancel'))
+        self.btnCancel = wx.Button(self, wx.ID_CANCEL, label=_('Cancel'))
         self.listBox = wx.ListBox(self)
 
-        labsizer1 = wx.BoxSizer(wx.HORIZONTAL)
-        labsizer1.Add(self.label1, 1, wx.ALL, 5)
+        # Layout
+        self.__DoLayout()
 
-        txtsizer = wx.BoxSizer(wx.HORIZONTAL)
-        txtsizer.Add(self.search, 1, wx.ALL, 5)
-
-        labsizer2 = wx.BoxSizer(wx.HORIZONTAL)
-        labsizer2.Add(self.label2, 1, wx.ALL, 5)
-
-        lstsizer = wx.BoxSizer(wx.HORIZONTAL)
-        lstsizer.Add(self.listBox, 1, wx.ALL, 5)
-
-        bsizer = wx.BoxSizer(wx.HORIZONTAL)
-        bsizer.AddStretchSpacer()
-        bsizer.Add(self.btnOk, flag=wx.ALL, border=5)
-        bsizer.Add(self.btnCancel, flag=wx.ALL, border=5)
-
-        mainsizer = wx.BoxSizer(wx.VERTICAL)
-        mainsizer.Add(labsizer1, flag=wx.FIXED_MINSIZE)
-        mainsizer.Add(txtsizer, flag=wx.FIXED_MINSIZE|wx.EXPAND)
-        mainsizer.Add(labsizer2, flag=wx.FIXED_MINSIZE)
-        mainsizer.Add(lstsizer, flag=wx.FIXED_MINSIZE|wx.EXPAND)
-        mainsizer.Add(bsizer, 1, wx.EXPAND)
-
-        self.SetSizer(mainsizer)
+        # Event Handlers
         self.Bind(wx.EVT_TEXT_ENTER, self.OnSearch, self.search)
         self.Bind(wx.EVT_SEARCHCTRL_CANCEL_BTN, self.OnCancelSearch, self.search)
         self.Bind(wx.EVT_BUTTON, self.OnConfirm, self.btnOk)
         self.Bind(wx.EVT_LISTBOX_DCLICK, self.OnConfirm, self.listBox)
         self.Bind(wx.EVT_BUTTON, self.OnCancel, self.btnCancel)
+
+    def __DoLayout(self):
+        """Layout the dialog"""
+        label1 = wx.StaticText(self, label=_('Find and open a module by name'))
+        labsizer1 = wx.BoxSizer(wx.HORIZONTAL)
+        labsizer1.Add(label1, 1, wx.ALL, 5)
+
+        txtsizer = wx.BoxSizer(wx.HORIZONTAL)
+        txtsizer.Add(self.search, 1, wx.ALL, 5)
+
+        dsizer = wx.BoxSizer(wx.HORIZONTAL)
+        divider = wx.StaticLine(self, size=(-1, 1))
+        dsizer.AddMany([((5, 5), 0), (divider, 1), ((5, 5), 0)])
+
+        label2 = wx.StaticText(self, label=_('Matching items') + u":")
+        labsizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        labsizer2.Add(label2, 1, wx.ALL, 5)
+
+        lstsizer = wx.BoxSizer(wx.HORIZONTAL)
+        lstsizer.Add(self.listBox, 1, wx.ALL|wx.EXPAND, 5)
+        lstvsizer = wx.BoxSizer(wx.VERTICAL)
+        lstvsizer.Add(lstsizer, 1, wx.EXPAND)
+
+        bsizer = wx.StdDialogButtonSizer()
+        bsizer.AddButton(self.btnOk)
+        bsizer.AddButton(self.btnCancel)
+        bsizer.Realize()
+
+        mainsizer = wx.BoxSizer(wx.VERTICAL)
+        mainsizer.AddMany([(labsizer1, 0), (txtsizer, 0, wx.EXPAND),
+                            ((5, 5), 0), (dsizer, 0, wx.EXPAND), ((5, 5), 0),
+                            (labsizer2, 0),
+                            (lstvsizer, 2, wx.EXPAND), (bsizer, 0, wx.EXPAND),
+                            ((5, 5), 0)])
+
+        self.SetSizer(mainsizer)
 
     def OnConfirm(self, evt):
         # TODO open file in editor (readonly?)
@@ -82,6 +107,7 @@ class OpenModuleDialog(wx.Dialog):
         self.listBox.Set([])
 
     def OnSearch(self, evt):
+        """Handle search events from the SearchCtrl"""
         files = self.FindModule(self.search.GetValue())
         if not files:
             self.listBox.Set([])
@@ -94,6 +120,7 @@ class OpenModuleDialog(wx.Dialog):
         """Return a list with the names of the source files that matched the
         the input line (actually,  an exact match at the moment, so a single
         file at most)
+
         """
         if not text:
             return None
@@ -126,6 +153,8 @@ class OpenModuleDialog(wx.Dialog):
 
         return [ fname ]
 
+#-----------------------------------------------------------------------------#
+# Test
 class MyApp(wx.App):
     def OnInit(self):
         dialog = OpenModuleDialog(None, "Find module")
