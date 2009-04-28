bb11260fec9269a5d36017faaaa173320a73cb80
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-04-28 02:00:18 +0000

    bump version number to 1.1

--- trunk/Projects/projects/__init__.py
+++ trunk/Projects/projects/__init__.py
@@ -5,7 +5,7 @@
 __author__ = "Kevin D. Smith <Kevin.Smith@sixquickrun.com>"
 __revision__ = "$Revision$"
 __scid__ = "$Id$"
-__version__ = "1.0"
+__version__ = "1.1"
 
 #-----------------------------------------------------------------------------#
 # Imports
