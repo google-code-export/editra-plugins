3c461dda0283ecbca835bfd357e9b92a27ad373c
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-05-06 12:10:55 +0000

    forgot to save before commit....

--- Projects/projects/BZR.py
+++ Projects/projects/BZR.py
@@ -211,7 +211,6 @@ class BZR(SourceControl):
                 # Split the status code and relative file path
                 code, fname = txt.split(None, 1)
                 fname = fname.replace(u'/', os.sep).strip().rstrip(os.sep)
-#                fname = fname.strip().rstrip(os.sep)
                 fname = fname.replace(relpath, '', 1).lstrip(os.sep)
                 code = code.rstrip('*')
 
