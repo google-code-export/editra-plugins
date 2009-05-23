b6cfddeb38d28ec022ce3840fd626636bff76177
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2009-05-23 00:48:58 +0000

    Bump version number

--- trunk/Projects/setup.py
+++ trunk/Projects/setup.py
@@ -6,7 +6,7 @@ __author__ = "Kevin D. Smith <Kevin.Smith@sixquickrun.com>"
 __revision__ = "$Revision$"
 __scid__ = "$Id$"
 __doc__ = "Projects"
-__version__ = "1.2"
+__version__ = "1.4"
 
 setup(
       name    = "Projects",
