d2f7e260aab58eb0ea06c9abacd71545e1e35979
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2010-11-12 05:04:03 +0000

    Get Submissions working

--- trunk/dailywtf/dailywtf/__init__.py
+++ trunk/dailywtf/dailywtf/__init__.py
@@ -5,7 +5,7 @@
 # Copyright: (c) 2010 Cody Precord <staff@editra.org>                         #
 # Licence: wxWindows Licence                                                  #
 ###############################################################################
-"""Adds Submit to TDWTF to the editor right click menu"""
+"""Adds "Submit to TDWTF" to the editor right click menu"""
 __author__ = "Cody Precord"
 __version__ = "0.1"
 
@@ -44,7 +44,8 @@ SUBMIT_TPL = """<Submit xmlns=\"http://thedailywtf.com/\">
 <subject>%s</subject>
 <comments>%s</comments>
 <codeSubmission>%s</codeSubmission>
-<doNotPublish>False</doNotPublish>
+<doNotPublish>false</doNotPublish>
+</Submit>
 """
 
 _ = wx.GetTranslation
@@ -116,9 +117,11 @@ def OnSubmit(buff, evt):
                 info = [data.encode('utf-8') for data in info]
             except UnicodeEncodeError:
                 return # FAIL TODO: report error to user
-            host = "http://thedailywtf.com/SubmitWTF.asmx"
+            host = "thedailywtf.com"
             bodytxt = SUBMIT_TPL % tuple(info)
-            message = ebmlib.SOAPMessage(host, bodytxt)
+            message = ebmlib.SOAP12Message(host, "/SubmitWTF.asmx", bodytxt,
+                                           "http://thedailywtf.com/Submit")
+            message.Send()
         else:
             pass # Canceled
     else:
@@ -221,119 +224,3 @@ class SubmitPanel(wx.Panel):
     @eclib.expose(SubmitDialog)
     def SetSenderEmail(self, email):
         self._email.SetValue(email)
-
-#(defun wtf-submit-button (&rest ignore)
-#  "Does the actual submit to the http://thedailywtf.com/SubmitWTF.asmx web service."
-#  (let ((xml (format "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-#<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">
-#<soap12:Body>
-#<Submit xmlns=\"http://thedailywtf.com/\">
-#<name>%s</name>
-#<emailAddress>%s</emailAddress>
-#<subject>%s</subject>
-#<comments>%s</comments>
-#<codeSubmission>%s</codeSubmission>
-#<doNotPublish>%s</doNotPublish>
-#</Submit>
-#</soap12:Body>
-#</soap12:Envelope>"
-#                     (widget-value wtf-name)
-#                     (widget-value wtf-email)
-#                     (widget-value wtf-subject)
-#                     (widget-value wtf-comment)
-#                     (widget-value wtf-source)
-#                     (if (widget-value wtf-dontPublish)
-#                         "true"
-#                       "false"))))
-#    (setq wtf-proc (http-post "http://thedailywtf.com/SubmitWTF.asmx"
-#                              xml
-#                              "application/soap+xml; charset=utf-8"
-#                              nil
-#                              'wtf-response-ignore))
-#    (kill-buffer (get-buffer-create "*submit-wtf*"))))
-
-#(defun http-post (url body content-type &optional headers sentinel
-#                      version verbose bufname)
-#  "Post to a URL in a buffer using HTTP 1.1, and return the process.
-#You can get the buffer associated with this process using
-#`process-buffer'. Shamelessly ripped from http-post.el
-
-#PARAMTERS has been replaces by body
-
-#CONTENT-TYPE is a coding system to use. Its upper case print name
-#will be used for the server. Possible values are `iso-8859-1' or
-#`euc-jp' and others.
-
-#The optional HEADERS are an alist where each element has the form
-#\(NAME . VALUE). Both must be strings and will be passed along with
-#the request. The reason CONTENT-TYPE is not just passed along as one
-#of the headers is that part of the Content-Type value is fixed and
-#cannot be changed: The basic encoding is implemented using
-#`html-url-encode' and is called application/x-www-form-urlencoded.
-
-#With optional argument SENTINEL, the buffer is not shown. It is the
-#responsability of the sentinel to show it, if appropriate. A sentinel
-#function takes two arguments, process and message. It is called when
-#the process is killed, for example. This is useful when specifying a
-#non-persistent connection. By default, connections are persistent.
-#Add \(\"Connection\" . \"close\") to HEADERS in order to specify a
-#non-persistent connection. Usually you do not need to specify a
-#sentinel, and `ignore' is used instead, to prevent a message being
-#printed when the connection is closed.
-
-#If you want to filter the content as it arrives, bind
-#`http-filter-pre-insert-hook' and `http-filter-post-insert-hook'.
-
-#The optional argument VERSION specifies the HTTP version to use. It
-#defaults to version 1.0, such that the connection is automatically
-#closed when the entire document has been downloaded.
-
-#If the optional argument VERBOSE is non-nil, a message will show the
-#command sent to the server.
-
-#The coding system of the process is set to `binary', because we need to
-#distinguish between \\r and \\n. To correctly decode the text later,
-#use `decode-coding-region' and get the coding system to use from
-#`http-headers'."
-#  (interactive)
-#  (setq version (or version 1.1))
-#  (let* (host dir file port proc buf header content-length)
-#    (unless (string-match
-#             "http://\\([^/:]+\\)\\(:\\([0-9]+\\)\\)?/\\(.*/\\)?\\([^:]*\\)"
-#             url)
-#      (error "Cannot parse URL %s" url))
-#    (unless bufname (setq bufname
-#                          (format "*HTTP POST %s *" url)))
-#    (setq host (match-string 1 url)
-#          port (or (and (setq port (match-string 3 url)) (string-to-int port)) 80)
-#          dir (or (match-string 4 url) "")
-#          file (or (match-string 5 url) "")
-#          buf (get-buffer-create bufname)
-#          proc (open-network-stream (concat "HTTP POST " url)
-#                                    buf (if http-proxy-host http-proxy-host host) (if http-proxy-port http-proxy-port port)))
-#    (set-process-sentinel proc (or sentinel 'ignore))
-#    (set-process-coding-system proc 'binary 'binary); we need \r\n in the headers!
-#    (set-process-filter proc 'http-filter)
-#    (set-marker (process-mark proc) (point-min) buf)
-#    (if sentinel
-#        (set-buffer buf)
-#      (switch-to-buffer buf))
-#    (erase-buffer)
-#    (kill-all-local-variables)
-    
-#    (setq header
-#          (concat (format "POST %s%s%s HTTP/%.1f\r\n"
-#                          (if http-proxy-host (concat "http://" host "/") "/")
-#                          dir
-#                          file
-#                          version)
-#                  (format "Host: %s\r\n" host)
-#                  (format "Content-Type: %s\r\n" content-type)
-#                  (format "Content-Length: %d\r\n" (length body))
-#                  "Connection: close\r\n"
-#                  "\r\n"))
-#    (when verbose
-#      (insert header body "\n\n"))
-#    (print (format "%s%s%s" header body "\n\n"))
-#    (process-send-string proc (concat header body "\r\n"))
-#    proc))
