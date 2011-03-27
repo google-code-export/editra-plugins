f85d2604cce679553985f111494da853ba48b233
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2011-03-27 21:12:06 +0000

    * Make strings translatable * Use UpdateUI events for managing button state * Simplify some of the layout * Fix string formatting issues

--- trunk/PyTools/PyTools/Debugger/AttachDialog.py
+++ trunk/PyTools/PyTools/Debugger/AttachDialog.py
@@ -33,23 +33,23 @@ _ = wx.GetTranslation
 
 class AttachDialog(eclib.ECBaseDlg):
     def __init__(self, parent):
-        super(AttachDialog, self).__init__(parent, wx.ID_ANY, "Attach")    
-        
+        super(AttachDialog, self).__init__(parent, title=_("Attach"))
+
         # Attributes
-        self.parent = parent
         self.m_server_list = None
         self.m_errors = {}
         self.m_index = None
-        
+
         # Layout
         sizerv = wx.BoxSizer(wx.VERTICAL)
 
-        desc = "Attach to a script (that has the debugger engine running) on local or remote machine:"
-        label = wx.StaticText(self, -1, desc, size = (350, -1))
+        desc = _("Attach to a script (that has the debugger engine running) on local or remote machine:")
+        label = wx.StaticText(self, label=desc, size=(350, -1))
         try:
             label.Wrap(350)
         except:
-            desc = """Attach to a script (that has the debugger engine 
+            # TODO: TRANSLATE _()
+            desc = """Attach to a script (that has the debugger engine
 running) on local or remote machine:"""
             label.SetLabel(desc)
 
@@ -58,88 +58,79 @@ running) on local or remote machine:"""
         sizerh = wx.BoxSizer(wx.HORIZONTAL)
         sizerv.Add(sizerh, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
 
-        label = wx.StaticText(self, -1, "Host:")
+        label = wx.StaticText(self, -1, _("Host:"))
         sizerh.Add(label, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
 
-        self.m_entry_host = wx.TextCtrl(self, value = self.parent._lasthost, size = (200, -1))
+        self.m_entry_host = wx.TextCtrl(self, value=self.Parent._lasthost, size = (200, -1))
         self.m_entry_host.SetFocus()
         sizerh.Add(self.m_entry_host, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
-        
-        btn = wx.Button(self, label = "Refresh")
+
+        btn = wx.Button(self, label = _("Refresh"))
         self.Bind(wx.EVT_BUTTON, self.do_refresh, btn) # for some reason, this must be up here otherwise window hangs
         btn.SetDefault()
         sizerh.Add(btn, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
 
-        self.m_listbox_scripts = eclib.EBaseListCtrl(parent = self, style = wx.LC_REPORT | wx.LC_SINGLE_SEL, size = (-1, 300))
-        self.m_listbox_scripts.InsertColumn(0, "PID" + '    ')
-        self.m_listbox_scripts.InsertColumn(1, "Filename")
-        
+        self.m_listbox_scripts = eclib.EBaseListCtrl(parent=self, style=wx.LC_REPORT|wx.LC_SINGLE_SEL, size = (-1, 300))
+        self.m_listbox_scripts.InsertColumn(0, _("PID") + '    ')
+        self.m_listbox_scripts.InsertColumn(1, _("Filename"))
+
         sizerv.Add(self.m_listbox_scripts, 0, wx.EXPAND | wx.ALL, 5)
 
-        btnsizer = wx.StdDialogButtonSizer()
+        btnsizer = self.CreateStdDialogButtonSizer(wx.OK|wx.CANCEL)
         sizerv.Add(btnsizer, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
-
-        self.m_ok = wx.Button(self, wx.ID_OK)
-        self.m_ok.Disable()
-        btnsizer.AddButton(self.m_ok)
-
-        btn = wx.Button(self, wx.ID_CANCEL)
-        btnsizer.AddButton(btn)
-        btnsizer.Realize()
-
         self.SetSizer(sizerv)
-        sizerv.Fit(self)
+        self.SetInitialSize()
 
         # Event Handlers
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
         self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnItemSelected, self.m_listbox_scripts)
         self.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.OnItemDeselected, self.m_listbox_scripts)
         self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnItemActivated, self.m_listbox_scripts)
-        
+        self.Bind(wx.EVT_UPDATE_UI,
+                  lambda evt: evt.Enable(bool(self.m_listbox_scripts.GetSelectedItemCount())),
+                  id=wx.ID_OK)
         wx.CallAfter(self.init2)
 
     def init2(self):
-        pwd_dialog = PasswordDialog(self, self.parent._lastpwd)
+        pwd_dialog = PasswordDialog(self, self.Parent._lastpwd)
         pos = self.GetPositionTuple()
         pwd_dialog.SetPosition((pos[0] + 50, pos[1] + 50))
-        r = pwd_dialog.ShowModal()
-        if r != wx.ID_OK:
+        if pwd_dialog.ShowModal() != wx.ID_OK:
             pwd_dialog.Destroy()
             self.Close()
             return
 
-        self.parent._lastpwd = pwd_dialog.get_password()
+        self.Parent._lastpwd = pwd_dialog.get_password()
         pwd_dialog.Destroy()
 
-        RPDBDEBUGGER.set_password(self.parent._lastpwd)            
+        RPDBDEBUGGER.set_password(self.Parent._lastpwd)
         self.do_refresh()
-                
+
     def set_cursor(self, id):
         cursor = wx.StockCursor(id)
-        self.SetCursor(cursor)        
-        self.m_listbox_scripts.SetCursor(cursor)        
+        self.SetCursor(cursor)
+        self.m_listbox_scripts.SetCursor(cursor)
 
     def OnCloseWindow(self, event):
-        self.m_ok = None
         event.Skip()
 
     def get_server(self):
         return self.m_server_list[self.m_index]
-        
+
     def do_refresh(self, event = None):
         host = self.m_entry_host.GetValue()
         if host == '':
             host = 'localhost'
-        self.parent._lasthost = host
+        self.Parent._lasthost = host
         worker = RunProcInThread("DbgAttach", self._onserverlist,
                                  RPDBDEBUGGER.get_server_list, host)
         worker.start()
-        
+
     def _onserverlist(self, res):
-        if not res or not self.m_ok:
+        if not res:
             return
         (self.m_server_list, self.m_errors) = res
-        
+
         if len(self.m_errors) > 0:
             for k, el in self.m_errors.items():
                 if k in [rpdb2.AuthenticationBadData, rpdb2.AuthenticationFailure]:
@@ -147,16 +138,14 @@ running) on local or remote machine:"""
 
                 elif k == rpdb2.EncryptionNotSupported:
                     self.report_attach_warning(rpdb2.STR_DEBUGGEE_NO_ENCRYPTION)
-                    
+
                 elif k == rpdb2.EncryptionExpected:
                     self.report_attach_warning(rpdb2.STR_ENCRYPTION_EXPECTED)
 
                 elif k == rpdb2.BadVersion:
                     for (t, v, tb) in el:
                         self.report_attach_warning(rpdb2.STR_BAD_VERSION % {'value': v})
-        
-        self.m_ok.Disable()
-            
+
         host = RPDBDEBUGGER.get_host()
         self.m_entry_host.SetValue(host)
 
@@ -164,28 +153,23 @@ running) on local or remote machine:"""
 
         for i, s in enumerate(self.m_server_list):
             index = self.m_listbox_scripts.InsertStringItem(sys.maxint, repr(s.m_pid))
-            
             self.m_listbox_scripts.SetStringItem(index, 1, s.m_filename)
             self.m_listbox_scripts.SetItemData(index, i)
 
     def report_attach_warning(self, warning):
-        dlg = wx.MessageDialog(self, warning, "Warning", wx.OK | wx.ICON_WARNING)
+        dlg = wx.MessageDialog(self, warning, _("Warning"),
+                               wx.OK|wx.ICON_WARNING)
         dlg.ShowModal()
-        dlg.Destroy()   
+        dlg.Destroy()
 
     def OnItemSelected(self, event):
-        self.m_index = event.m_itemIndex
-        self.m_ok.Enable()
-
+        self.m_index = event.GetIndex()
         event.Skip()
 
     def OnItemDeselected(self, event):
-        if self.m_listbox_scripts.GetSelectedItemCount() == 0:
-            self.m_ok.Disable()
+        event.Skip()
 
-        event.Skip()    
-        
     def OnItemActivated(self, event):
-        self.m_index = event.m_itemIndex
+        self.m_index = event.GetIndex()
 
         self.EndModal(wx.ID_OK)
