55cf44ec185877d24792435abb0e6862fd2cfc2d
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2011-04-01 02:53:07 +0000

    Use markers instead of indicators. Allow multiple buffers to have lint markers and hover over actions at the same time.

--- PyTools/PyTools/SyntaxChecker/CheckResultsList.py
+++ PyTools/PyTools/SyntaxChecker/CheckResultsList.py
@@ -15,7 +15,6 @@ __revision__ = "$Revision$"
 #----------------------------------------------------------------------------#
 # Imports
 import wx
-from wx.stc import STC_INDIC_SQUIGGLE
 
 # Editra Libraries
 import ed_msg
@@ -30,14 +29,17 @@ _ = wx.GetTranslation
 #----------------------------------------------------------------------------#
 
 class CheckResultsList(eclib.EBaseListCtrl):
-    """List control for displaying syntax check results"""
+    """List control for displaying syntax check results
+    @todo: decouple marks and data from UI
+
+    """
+    _cache = dict()
     def __init__(self, parent):
         super(CheckResultsList, self).__init__(parent)
 
         # Attributes
         self.editor = None
-        self.showedtip = False
-        self.errorlines = dict()
+        self._mw = None
 
         # Setup
         self.InsertColumn(0, _("Type"))
@@ -55,9 +57,10 @@ class CheckResultsList(eclib.EBaseListCtrl):
     def OnDestroy(self, evt):
         if evt.GetEventObject() is self:
             ed_msg.Unsubscribe(self.OnDwellStart)
+            self.ClearMarkers()
 
     def set_mainwindow(self, mw):
-        self._mainw = mw
+        self._mw = mw
 
     def set_editor(self, editor):
         self.editor = editor
@@ -66,67 +69,114 @@ class CheckResultsList(eclib.EBaseListCtrl):
         """Show calltips for the error if dwelling over a line"""
         data = msg.GetData()
         buf = data.get('stc', None)
-        if buf and buf == self.editor:
+        if buf:
             lineno = data.get('line', -1)
-            errorlist = self.errorlines.get(lineno)
-            if errorlist:
-                data['rdata'] = "\n".join(errorlist)
+            fname = buf.GetFileName()
+            if fname in CheckResultsList._cache:
+                errorlist = CheckResultsList._cache[fname].GetLineData(lineno)
+                if errorlist and len(errorlist):
+                    errors = [ "%s %s" % err for err in errorlist ]
+                    data['rdata'] = "\n".join(errors)
 
     def OnItemActivate(self, evt):
         """Go to the error in the file"""
-        if not self.editor:
-            return
-        idx = evt.GetIndex()
-        itm = self.GetItem(idx, 1).GetText()
-        try:
-            lineNo = int(itm)
-            self.editor.GotoLine(max(0, lineNo - 1))
-        except ValueError:
-            pass
+        editor = wx.GetApp().GetCurrentBuffer()
+        if editor:
+            idx = evt.GetIndex()
+            itm = self.GetItem(idx, 1).GetText()
+            try:
+                lineNo = int(itm)
+                editor.GotoLine(max(0, lineNo - 1))
+            except ValueError:
+                pass
 
     def Clear(self):
         """Delete all the rows """
-        if not self.editor:
-            return
-        self.DeleteAllItems()
-        PyToolsUtils.unset_indic(self.editor)
-        self.errorlines = {}
+        if self.editor:
+            fname = self.editor.GetFileName()
+            if fname in CheckResultsList._cache:
+                del CheckResultsList._cache[fname]
+            self.DeleteAllItems()
+            self.editor.RemoveAllLintMarks()
 
-    def PopulateRows(self, data):
-        """Populate the list with the data
-        @param data: list of tuples (errorType, errorText, errorLine)
+    def ClearMarkers(self):
+        if self._mw:
+            nb = self._mw.GetNotebook()
+            ctrls = nb.GetTextControls()
+            for ctrl in ctrls:
+                if ctrl.GetFileName() in CheckResultsList._cache:
+                    ctrl.RemoveAllLintMarks()
+
+    def LoadData(self, data):
+        """Load data into the cache and display it in the list
+        @param fname: filename
+        @param data: Lint data [(errorType, errorText, errorLine),]
 
         """
         if not self.editor:
-            return
-        self.editor.IndicatorSetStyle(2, STC_INDIC_SQUIGGLE)
-        self.editor.IndicatorSetForeground(2, 'red')
+            return # TODO: Log
+        fname = self.editor.GetFileName()
+        CheckResultsList._cache[fname] = LintData(data)
+        self._PopulateRows(CheckResultsList._cache[fname])
 
+    def _PopulateRows(self, data):
+        """Populate the list with the data
+        @param data: LintData object
+
+        """
         typeText = _("Type")
         errorText = _("Error")
         minLType = max(self.GetTextExtent(typeText)[0], self.GetColumnWidth(0))
         minLText = max(self.GetTextExtent(errorText)[0], self.GetColumnWidth(2))
-        self.errorlines = {}
-        self._data = {}
-        for idx, (eType, eText, eLine) in enumerate(data):
-            eText = unicode(eText).rstrip()
-            self._data[idx] = (unicode(eType), unicode(eLine), eText)
-            minLType = max(minLType, self.GetTextExtent(eType)[0])
-            minLText = max(minLText, self.GetTextExtent(eText)[0])
-            self.Append(self._data[idx])
-            self.SetItemData(idx, idx)
-
+        for row in data.GetOrderedData():
+            assert len(row) == 3
+            minLType = max(minLType, self.GetTextExtent(row[0])[0])
+            minLText = max(minLText, self.GetTextExtent(row[2])[0])
+            self.Append(row)
             try:
-                lineNo = int(eLine)
-                errorlist = self.errorlines.get(lineNo)
-                if not errorlist:
-                    errorlist = []
-                    self.errorlines[lineNo] = errorlist
-                errorlist.append(eText)
-                if eType == "Error":
-                    PyToolsUtils.set_indic(lineNo - 1, self.editor)
+                self.editor.AddLintMark(int(row[1]) - 1) # TODO: store handles
             except ValueError:
                 pass
 
         self.SetColumnWidth(0, minLType)
         self.SetColumnWidth(2, minLText)
+
+#-----------------------------------------------------------------------------#
+
+class LintData(object):
+    def __init__(self, data):
+        """@param data: [(type, text, line),]"""
+        super(LintData, self).__init__()
+
+        # Attributes
+        self._data = dict() # lineno -> [(errorType, errorText),]
+
+        # Setup
+        for val in data:
+            assert len(val) == 3
+            line = val[2]
+            if line not in self._data:
+                self._data[line] = list()
+            self._data[line].append((unicode(val[0]), unicode(val[1]).rstrip()))
+
+    def GetOrderedData(self):
+        """Iterate over the data ordered by line number
+        @return: [(errType, line, errText),]
+
+        """
+        rdata = list()
+        for key in sorted(self._data.keys()):
+            for data in self._data[key]:
+                if isinstance(key, basestring):
+                    rdata.insert(0, (data[0], unicode(key), data[1]))
+                else:
+                    rdata.append((data[0], unicode(key), data[1]))
+        return rdata
+
+    def GetLineData(self, line):
+        """Get data for the given line
+        @param line: line number (int)
+        @return: [(errType, errText),] or None
+
+        """
+        return self._data.get(line, None)
