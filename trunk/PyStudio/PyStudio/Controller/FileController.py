53a16f4ed299af45e3b1e490c2b15e8b64212259
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2011-10-16 18:48:44 +0000

    * Add Controller module * Add FileController base class for managing file actions in projects view. * Implement New File / New Folder methods in project view using new FileController interface.

---
+++ trunk/PyStudio/PyStudio/Controller/FileController.py
@@ -0,0 +1,65 @@
+###############################################################################
+# Name: FileController.py                                                     #
+# Purpose: Project File Manager                                               #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2011 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+"""
+File Controller Base Class
+
+Defines base interface for file controllers used by the ProjectTree user
+interface.
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#-----------------------------------------------------------------------------#
+# Dependencies
+import os
+import sys
+
+# Editra Imports
+import ebmlib
+
+#-----------------------------------------------------------------------------#
+
+class FileController(ebmlib.FactoryMixin):
+    """Base factory for file controllers"""
+    def __init__(self):
+        super(FileController, self).__init__()
+
+        # Attributes
+        
+    @classmethod
+    def GetMetaDefaults(cls):
+        """Return mapping of default meta-data
+        base class implements the controller for the operating systems file
+        system. Subclasses should define their own nested `meta` class that
+        defines the `system` attribute which is used to identify the appropriate
+        controller to use.
+
+        """
+        return dict(system="OS")
+
+    #---- Interface Implementation ----#
+
+    def CreateFile(self, path, name):
+        """Create a new file at the given path
+        @param path: directory path
+        @param name: file name
+
+        """
+        ebmlib.MakeNewFile(path, name)
+
+    def CreateFolder(self, path, name):
+        """Create a new folder at the given path
+        @param path: directory path
+        @param name: folder name
+
+        """
+        ebmlib.MakeNewFolder(path, name)
--- trunk/PyStudio/PyStudio/Project/ProjectFile.py
+++ trunk/PyStudio/PyStudio/Project/ProjectFile.py
@@ -19,6 +19,9 @@ Project Data class representing the Project file
 # Imports
 import os
 
+# Local Imports
+import PyStudio.Project.ProjectXml as ProjectXml
+
 #-----------------------------------------------------------------------------#
 
 class ProjectFile(object):
@@ -32,10 +35,21 @@ class ProjectFile(object):
         super(ProjectFile, self).__init__()
 
         # Attributes
-        self._pxml = pxml
-        self._path = path
+        self._pxml = pxml  # Project file xml data
+        self._path = path  # Project file path
+        # Derived attributes
+        self._optmap = dict() # Project option map
+
+        # Setup
+        self.__RefreshOptionMap()
+
+    def __RefreshOptionMap(self):
+        """Build the option map"""
+        for opt in self._pxml.options:
+            self._optmap[opt.name] = opt
 
     #---- Properties ----#
+
     Path = property(lambda self: self._path)
     ProjectRoot = property(lambda self: os.path.dirname(self.Path))
 
@@ -44,3 +58,31 @@ class ProjectFile(object):
     def Save(self):
         """Save the project file to disk"""
         self._pxml.Write(self.Path)
+
+    #---- Accessors ----#
+
+    def GetOption(self, optname):
+        """Get the value for a project option
+        @param optname: option name
+        @return: option value or None
+
+        """
+        option = self._optmap.get(optname, None)
+        if option:
+            option = option.value
+        return option
+
+    def SetOption(self, optname, value):
+        """Set a project option
+        @param optname: option name
+        @param value: option value
+
+        """
+        if optname in self._optmap:
+            self._optmap[optname].value = value
+        else:
+            # New option
+            nopt = ProjectXml.Option(name=optname, value=value)
+            self._pxml.options.append(nopt)
+            self.__RefreshOptionMap()
+        
