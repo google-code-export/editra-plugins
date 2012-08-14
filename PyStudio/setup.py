9f45e7818898de2355a8433310f7ab8cb10b91e5
CodyPrecord <CodyPrecord@9e3eaefd-cf36-0410-9872-65c0d5819ccb>
2012-08-14 19:54:07 +0000

    bump version numbers to 0.6

--- PyStudio/setup.py
+++ PyStudio/setup.py
@@ -19,7 +19,7 @@ import os
 if setup != None:
     setup(
         name='PyStudio',
-        version='0.5',
+        version='0.6',
         description=__doc__,
         author=__author__,
         author_email="rans@email.com",
